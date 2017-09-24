d16e8bc990568d1c938b386b920b4ff52daaa401
Ray Vafa <vafatabar@gmail.com>
2017-09-25 00:15:13 +1000

    common header component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 // This is the Link API
-import Link from 'next/link'
+import Link from 'next/link';
+import Header from '../components/Header';
 
 const Index = () => (
   <div>
-    <Link href="/about">
-      <a style={{ fontSize: 20 }}>About Page</a>
-    </Link>
+    <Header />
     <p>Hello Next.js</p>
   </div>
 );
